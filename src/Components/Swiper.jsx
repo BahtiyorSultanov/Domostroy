7eb6856a2d54e90bcdf67dbcfee8a56db798f87b
BahtiyorSultanov <sbahtiyour@gmail.com>
2023-08-31 16:26:30 +0500

    test for phone

--- src/Components/Swiper.jsx
+++ src/Components/Swiper.jsx
@@ -7,7 +7,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import { NavLink } from "react-router-dom";
 
-function Swipper({ data, isLink, slidesPerView, space, additionallClass}) {
+function Swipper({ data, isLink, slidesPerView, space, additionallClass }) {
   return (
     <div>
       <Swiper
@@ -25,6 +25,22 @@ function Swipper({ data, isLink, slidesPerView, space, additionallClass}) {
         modules={[Autoplay, Pagination, Navigation]}
         className={`mySwiper ${additionallClass}`}
       >
+        <SwiperSlide>
+          <NavLink to={`/bestSellerItem/1`}>
+            <div className="flex items-center">
+              <div className="flex flex-col items-start w-[234px] border-[2px] gap-[20px]">
+                <img src="" className="w-[100%] h-[100%]" />
+                <ul className="flex flex-col items-start gap-[10px] pl-[30px]">
+                  <h1 className="text-[#49423D] text-[16[px]">Test</h1>
+                  <li className="text-[14px] text-[#909090] text-left">Test</li>
+                  <h1 className="text-[20px] font-bold text-[#8DB338] max-sm:text-[16px]">
+                    Test
+                  </h1>
+                </ul>
+              </div>
+            </div>
+          </NavLink>
+        </SwiperSlide>
         {data &&
           data.map((item) => {
             return (
@@ -64,7 +80,9 @@ function Swipper({ data, isLink, slidesPerView, space, additionallClass}) {
                     <div className="flex flex-col items-start w-[234px] border-[2px] gap-[20px]">
                       {item.img ? (
                         <img src={item.img} className="w-[100%] h-[100%]" />
-                      ):<img src={item} className="w-[100%] h-[100%]"/>}
+                      ) : (
+                        <img src={item} className="w-[100%] h-[100%]" />
+                      )}
                       <ul className="flex flex-col items-start gap-[10px] pl-[30px]">
                         {item.h1 && (
                           <h1 className="text-[#49423D] text-[16[px]">
